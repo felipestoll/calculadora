2d97e4f0053423572819468627556b4205191f97
felipestoll <57003112+felipestoll@users.noreply.github.com>
2025-08-19 15:34:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def calcular():
     except:
         return jsonify({'resultado': 'Erro'})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
